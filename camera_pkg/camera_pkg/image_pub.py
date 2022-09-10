6f05d2a59cbb355bbc53d769ad4c693bca69269c
양동현 <80799025+YDongHyun@users.noreply.github.com>
2022-09-10 18:14:44 +0900

    Add files via upload

---
+++ camera_pkg/camera_pkg/image_pub.py
@@ -0,0 +1,36 @@
+
+import rclpy
+import cv2
+from rclpy.node import Node
+from rclpy.qos import QoSProfile
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+
+class Image_pub(Node):
+    def __init__(self):
+        super().__init__('image_pub')
+        qos_profile = QoSProfile(depth=10)
+        self.image_pub=self.create_publisher(Image,'/image',qos_profile)
+        self.img_data = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
+        self.cv_bridge = CvBridge()
+
+    def publish_image_msg(self):
+        msg=Image()
+        msg=self.cv_bridge.cv2_to_imgmsg(self.img_data)
+        self.image_pub.publish(msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node=Image_pub()
+    try:
+        rclpy.spin(node)
+
+    except KeyboardInterrupt:
+        node.get_logger().info('Keyboard Interrupt (SIGINT)')
+
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+if __name__=='__main__':
+    main()
---
+++ camera_pkg/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'camera_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ydh',
+    maintainer_email='ydh@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        'image_pub = camera_pkg.image_pub:main',
+        ],
+    },
+)
