72502834718a8607a4aec59014e2eb13e1739c05
양동현 <80799025+YDongHyun@users.noreply.github.com>
2022-09-13 21:19:29 +0900

    Update image_pub.py

--- camera_pkg/camera_pkg/image_pub.py
+++ camera_pkg/camera_pkg/image_pub.py
@@ -1,4 +1,3 @@
-
 import rclpy
 import cv2
 from rclpy.node import Node
@@ -10,21 +9,21 @@ class Image_pub(Node):
     def __init__(self):
         super().__init__('image_pub')
         qos_profile = QoSProfile(depth=10)
-        self.image_pub=self.create_publisher(CompressedImage,'/image',qos_profile)
+        self.image_pub=self.create_publisher(CompressedImage,'/CompressedImage',qos_profile)
         self.cv_bridge = CvBridge()
         self.timer=self.create_timer(1,self.publish_image_msg)
 
     def publish_image_msg(self): 
         msg=CompressedImage()
-        vid_data = cv2.VideoCapture('/dev/video0')
+        vid_data = cv2.VideoCapture('/dev/video0',cv2.CAP_V4L)
         vid_data.set(3,640)
         vid_data.set(4,480)
         #while(vid_data.isOpened()):
-        ret,image=vid_data.read()
-        if ret:
-            msg=self.cv_bridge.cv2_to_compressed_imgmsg(image)
-            self.image_pub.publish(msg)
-            print("published")
+        ret,image=vid_data.read()            
+        msg=self.cv_bridge.cv2_to_compressed_imgmsg(image)
+        self.image_pub.publish(msg)
+        print("published")
+        cap.release()
 
 def main(args=None):
     rclpy.init(args=args)
