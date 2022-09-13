a22e0d47676c38739b3b9277bd599be86d0518e4
양동현 <80799025+YDongHyun@users.noreply.github.com>
2022-09-13 21:32:21 +0900

    Update image_pub.py

--- camera_pkg/camera_pkg/image_pub.py
+++ camera_pkg/camera_pkg/image_pub.py
@@ -2,19 +2,19 @@ import rclpy
 import cv2
 from rclpy.node import Node
 from rclpy.qos import QoSProfile
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge
 
 class Image_pub(Node):
     def __init__(self):
         super().__init__('image_pub')
         qos_profile = QoSProfile(depth=10)
-        self.image_pub=self.create_publisher(Image,'/Image',qos_profile)
+        self.image_pub=self.create_publisher(CompressedImage,'/CompressedImage',qos_profile)
         self.cv_bridge = CvBridge()
         self.timer=self.create_timer(1,self.publish_image_msg)
 
     def publish_image_msg(self): 
-        msg=Image()
+        msg=CompressedImage()
         vid_data = cv2.VideoCapture('/dev/video0',cv2.CAP_V4L)
         vid_data.set(3,640)
         vid_data.set(4,480)
