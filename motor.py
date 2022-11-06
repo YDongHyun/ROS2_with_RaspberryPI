ce45517375b6a92617c0bfc50cce8dd0758ffedb
양동현 <80799025+YDongHyun@users.noreply.github.com>
2022-11-06 19:11:27 +0900

    Update motor.py

--- motor.py
+++ motor.py
@@ -1,23 +1,11 @@
-from gpiozero import Motor
-import time
 import keyboard
-
-
-motor_r = Motor(forward=12,backward=16)
-motor_l = Motor(forward=21,backward=22)
-
+ 
 while True:
-    if keyboard.read_key() == "w":
-        motor_r.foward(speed=0.5)
-        motor_l.foward(speed=0.5)
-    elif keyboard.read_key() == "a":
-        motor_r.foward(speed=0.5)
-        motor_l.backward(speed=0.5)
-    elif keyboard.read_key() == "s":
-        motor_r.backward(speed=0.5)
-        motor_l.backward(speed=0.5)
-    elif keyboard.read_key() == "d":
-        motor_r.backward(speed=0.5)
-        motor_l.foward(speed=0.5)
+    key=keyboard.read_key()
+    if key=="q":
+        print("press")
+        break
     else:
-        continue
+        if key:
+            print(key)
+            key=False
