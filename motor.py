1c086907eb48468c8164d6a16c3f1e2f80293b21
양동현 <80799025+YDongHyun@users.noreply.github.com>
2022-11-08 19:14:46 +0900

    Update motor.py

--- motor.py
+++ motor.py
@@ -1,11 +1,20 @@
-import keyboard
- 
-while True:
-    key=keyboard.read_key()
-    if key=="q":
-        print("press")
-        break
-    else:
-        if key:
-            print(key)
-            key=False
+import time
+from sshkeyboard import listen_keyboard
+from gpiozero import Motor
+
+motor_r=Motor(forward=12,backward=16)
+motor_l=Motor(forward=20,backward=21)
+
+def press(key):
+    if key=="w":
+        motor_r.forward(speed=0.5)
+        motor_l.forward(speed=0.5)
+    elif key=="a":
+        motor_r.forward(speed=0.5)
+        motor_l.backward(speed=0.5)
+    elif key=="s":
+        motor_r.backward(speed=0.5)
+        motor_l.backward(speed=0.5)
+    elif key=="d":
+        motor_r.backward(speed=0.5)
+        motor_l.forward(speed=0.5)
